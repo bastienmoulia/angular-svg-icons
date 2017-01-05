edfc8b2ce3e1d8814d2765fc84c75f18caeb5a35
Bastien <bmoulia@gmail.com>
2017-01-05 20:09:17 +0100

    Add svg exemple and improve ts and scss

--- dist/angular-svg-icons.js
+++ dist/angular-svg-icons.js
@@ -1,15 +1,20 @@
 (function () {
-    'use strict';
+    "use strict";
     var SvgIconController = (function () {
         function SvgIconController($svgIcon, $log) {
             this.$svgIcon = $svgIcon;
             this.$log = $log;
             this.url = "";
-            console.log("ok2");
         }
         SvgIconController.prototype.$onInit = function () {
             if (this.name) {
-                this.url = this.$svgIcon.spritesPath + "#" + this.name;
+                this.url = this.$svgIcon.spritesFile + "#" + this.name;
+                if (!this.height) {
+                    this.height = this.$svgIcon.svgHeight;
+                }
+                if (!this.width) {
+                    this.width = this.height;
+                }
             }
             else {
                 this.$log.debug("no attribute name for svg-icon");
@@ -19,21 +24,31 @@
     }());
     angular
         .module("angular-svg-icons", [])
-        .provider('$svgIcon', function () {
-        var spritesPath = "sprites.svg";
+        .provider("$svgIcon", function () {
+        var spritesFile = "sprites.svg";
+        var svgFolder = "svg";
+        var svgHeight = 15;
         return {
-            spritesPath: function (value) {
-                spritesPath = value;
+            spritesFile: function (value) {
+                spritesFile = value;
+            },
+            svgFolder: function (value) {
+                svgFolder = value;
+            },
+            svgHeight: function (value) {
+                svgHeight = value;
             },
             $get: function () {
                 return {
-                    spritesPath: spritesPath
+                    spritesFile: spritesFile,
+                    svgFolder: svgFolder,
+                    svgHeight: svgHeight
                 };
             }
         };
     })
-        .component('svgIcon', {
-        template: "\n          <svg ng-if=\"svgIcon.url != ''\">\n            <use ng-attr-xlink:href=\"{{ svgIcon.url }}\" xlink:href=\"\">\n          </svg>\n        ",
+        .component("svgIcon", {
+        template: "\n        <svg ng-if=\"svgIcon.url !== '' && !svgIcon.isIe\" ng-attr-height=\"{{ svgIcon.height }}\" ng-attr-width=\"{{ svgIcon.width }}\" xmlns=\"http://www.w3.org/2000/svg\">\n          <use ng-attr-xlink:href=\"{{ svgIcon.url }}\" xlink:href=\"\">\n        </svg>\n        <img ng-src=\"{{ svgIcon.url }}\" alt=\"{{ svgIcon.name }}\" ng-attr-height=\"{{ svgIcon.height }}\" ng-attr-width=\"{{ svgIcon.width }}\" ng-if=\"svgIcon.url !== '' && svgIcon.isIe\">\n      ",
         controllerAs: "svgIcon",
         controller: SvgIconController,
         bindings: {
--- src/angular-svg-icons.ts
+++ src/angular-svg-icons.ts
@@ -1,68 +1,68 @@
 (function() {
-    "use strict";
+  "use strict";
 
-    class SvgIconController {
-      name: string;
-      url: string;
-      height: number;
-      width: number;
-      constructor(private $svgIcon: any, private $log: angular.ILogService) {
-        this.url = "";
-      }
-      $onInit() {
-        if (this.name) {
-          this.url = this.$svgIcon.spritesFile + "#" + this.name;
-          if (!this.height) {
-            this.height = this.$svgIcon.svgHeight;
-          }
-          if (!this.width) {
-            this.width = this.height;
-          }
-        } else {
-          this.$log.debug("no attribute name for svg-icon")
+  class SvgIconController {
+    name: string;
+    url: string;
+    height: number;
+    width: number;
+    constructor(private $svgIcon: any, private $log: angular.ILogService) {
+      this.url = "";
+    }
+    $onInit() {
+      if (this.name) {
+        this.url = this.$svgIcon.spritesFile + "#" + this.name;
+        if (!this.height) {
+          this.height = this.$svgIcon.svgHeight;
         }
+        if (!this.width) {
+          this.width = this.height;
+        }
+      } else {
+        this.$log.debug("no attribute name for svg-icon")
       }
     }
+  }
 
-    angular
-      .module("angular-svg-icons", [])
-      .provider("$svgIcon", function () {
-        let spritesFile: string = "sprites.svg";
-        let svgFolder: string = "svg";
-        let svgHeight: number = 15;
-        return {
-          spritesFile: (value: string) => {
-            spritesFile = value;
-          },
-          svgFolder: (value: string) => {
-            svgFolder = value;
-          },
-          svgHeight: (value: number) => {
-            svgHeight = value;
-          },
+  angular
+    .module("angular-svg-icons", [])
+    .provider("$svgIcon", function () {
+      let spritesFile: string = "sprites.svg";
+      let svgFolder: string = "svg";
+      let svgHeight: number = 15;
+      return {
+        spritesFile: (value: string) => {
+          spritesFile = value;
+        },
+        svgFolder: (value: string) => {
+          svgFolder = value;
+        },
+        svgHeight: (value: number) => {
+          svgHeight = value;
+        },
 
-          $get: () => {
-            return {
-              spritesFile: spritesFile,
-              svgFolder: svgFolder,
-              svgHeight: svgHeight
-            };
-          }
+        $get: () => {
+          return {
+            spritesFile: spritesFile,
+            svgFolder: svgFolder,
+            svgHeight: svgHeight
+          };
         }
-      })
-      .component("svgIcon", {
-        template: `
-          <svg ng-if="svgIcon.url != """>
-            <use ng-attr-xlink:href="{{ svgIcon.url }}" xlink:href="">
-          </svg>
-        `,
-        controllerAs: "svgIcon",
-        controller: SvgIconController,
-        bindings: {
-          name: "<",
-          height: "<",
-          width: "<"
-        }
-      });
-
+      }
+    })
+    .component("svgIcon", {
+      template: `
+        <svg ng-if="svgIcon.url !== '' && !svgIcon.isIe" ng-attr-height="{{ svgIcon.height }}" ng-attr-width="{{ svgIcon.width }}" xmlns="http://www.w3.org/2000/svg">
+          <use ng-attr-xlink:href="{{ svgIcon.url }}" xlink:href="">
+        </svg>
+        <img ng-src="{{ svgIcon.url }}" alt="{{ svgIcon.name }}" ng-attr-height="{{ svgIcon.height }}" ng-attr-width="{{ svgIcon.width }}" ng-if="svgIcon.url !== '' && svgIcon.isIe">
+      `,
+      controllerAs: "svgIcon",
+      controller: SvgIconController,
+      bindings: {
+        name: "<",
+        height: "<",
+        width: "<"
+      }
+    });
 })();
